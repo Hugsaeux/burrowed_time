8aebb1fa5eec8a093d840c50ffa32d889552edba
Sudikoff Lab iMac <imac@dhcp-210-222.lab.cs.dartmouth.edu>
2017-05-04 19:14:17 -0400

    can only add 7 locations to a group now

--- Burrowed Time/LocationPickerTableViewCell.swift
+++ Burrowed Time/LocationPickerTableViewCell.swift
@@ -18,7 +18,7 @@ class LocationPickerTableViewCell: UITableViewCell {
     var cellIndex:Int!
     
     @IBAction func switchChanged(_ sender: Any) {
-        if (groupList.groups[cellGroupIndex].locations.count < 6) {
+        if (groupList.groups[cellGroupIndex].locations.count < 7) {
             if (!groupList.groups[cellGroupIndex].checkLocation(location: cellLocation)) {
                 let location:Location = Location(name: cellLocation, locationID: cellIndex)
                 groupList.groups[cellGroupIndex].addLocation(location: location)
@@ -26,38 +26,40 @@ class LocationPickerTableViewCell: UITableViewCell {
             else {
                 groupList.groups[cellGroupIndex].removeLocation(location: cellLocation)
             }
-            
-            var locations = [Int]()
-    //        for location in groupList.groups[cellGroupIndex].locations {
-    //            locations.append(location.locationID)
-    //        }
-            
-            // need identifier from location util
-            let storedRegionLookup = RegionLookup()
-            storedRegionLookup.loadRegionLookupFromPhone()
-            for region in locationUtil!.manager.monitoredRegions {
-                for location in groupList.groups[cellGroupIndex].locations {
-                    // Make a new annotation for this region
-                    let regionIdx = region.identifier
-                    let regionInfo:NSArray = storedRegionLookup.regionLookup.object(forKey: regionIdx) as! NSArray
-                    let title = String(describing: regionInfo[TITLE])
-                    
-                    if (title == location.name) {
-                        locations.append(Int(regionIdx)!)
-                        location.locationID = Int(regionIdx)!
-                    }
+        }
+        else if (groupList.groups[cellGroupIndex].locations.count == 7) {
+            if (trackingSwitch.isOn == false) {
+                groupList.groups[cellGroupIndex].removeLocation(location: cellLocation)
+            }
+            else {
+                trackingSwitch.setOn(false, animated: true)
+            }
+        }
+        
+        var locations = [Int]()
+        
+        // need identifier from location util
+        let storedRegionLookup = RegionLookup()
+        storedRegionLookup.loadRegionLookupFromPhone()
+        for region in locationUtil!.manager.monitoredRegions {
+            for location in groupList.groups[cellGroupIndex].locations {
+                // Make a new annotation for this region
+                let regionIdx = region.identifier
+                let regionInfo:NSArray = storedRegionLookup.regionLookup.object(forKey: regionIdx) as! NSArray
+                let title = String(describing: regionInfo[TITLE])
+                
+                if (title == location.name) {
+                    locations.append(Int(regionIdx)!)
+                    location.locationID = Int(regionIdx)!
                 }
             }
-            
-            let api:API = API()
-            api.change_group_locations(groupid: groupList.groups[cellGroupIndex].getIdentifier(), locs: locations as NSArray)
-            
-            
-            groupList.saveGroupListToPhone()
         }
-        else {
-            trackingSwitch.onTintColor = UIColor .red
-        }
+        
+        let api:API = API()
+        api.change_group_locations(groupid: groupList.groups[cellGroupIndex].getIdentifier(), locs: locations as NSArray)
+        
+        
+        groupList.saveGroupListToPhone()
     }
 
     override func awakeFromNib() {
