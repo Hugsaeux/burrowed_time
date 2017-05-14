e42d29fcd634750be2bdc1b533feb0a2f13e4f01
Hugsaeux <hughes.lee.17@dartmouth.edu>
2017-05-14 17:06:01 -0400

    Added a single function for updateCurrentLocation to server Phone now checks location and updates server every time the app becomes active

--- Burrowed Time/MapGUIViewController.swift
+++ Burrowed Time/MapGUIViewController.swift
@@ -23,6 +23,33 @@ class LocationAnnotation: NSObject, MKAnnotation{
     }
 }
 
+func updateCurrentLocation(){
+    let storedRegionLookup = RegionLookup()
+    storedRegionLookup.loadRegionLookupFromPhone()
+    let api:API = API()
+    
+    for region in locationUtil!.manager.monitoredRegions {
+        // Make a new annotation for this region
+        let regionIdx = region.identifier
+        let regionInfo:NSArray = storedRegionLookup.regionLookup.object(forKey: regionIdx) as! NSArray
+        
+        //check if in bounds
+        let latitude = NumberFormatter().number(from: String(describing: regionInfo[LATITUDE]))!.doubleValue
+        let longitude = NumberFormatter().number(from: String(describing: regionInfo[LONGITUDE]))!.doubleValue
+        let radius = NumberFormatter().number(from: String(describing: regionInfo[RADIUS]))!.doubleValue
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let ourLocation = locationUtil!.manager.location
+        let clLocCoor = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let distance = ourLocation?.distance(from: clLocCoor)
+        
+        if ((distance! as Double) < radius) {
+            api.enter_location(loc_num: regionIdx)
+        }else{
+            api.exit_location(loc_num: regionIdx)
+        }
+    }
+}
+
 class MapGUIViewController: UIViewController, MKMapViewDelegate {
     var currentTitle:String!
     var currentRadius:CLLocationDistance = 100
@@ -283,6 +310,7 @@ class MapGUIViewController: UIViewController, MKMapViewDelegate {
         else if (segue.identifier == "saveNewPlace") {
             let storedRegionLookup = RegionLookup()
             storedRegionLookup.loadRegionLookupFromPhone()
+            
             for region in locationUtil!.manager.monitoredRegions {
                 // Make a new annotation for this region
                 let regionIdx = region.identifier
